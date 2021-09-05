acb9150c14eead108f03a842623b26e4157d348b
@rafavg77 <rafavg77@gmail.com>
2021-09-05 02:42:21 -0500

    configuration9

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "[+] Stopping bot_telegram_pokedex service"
 sudo service bot_telegram_pokedex stop
+echo "[+] waiting ..."
 sleep 60
+echo "[+] Accessing to directory"
 cd /home/pi/Production/Bots/PokedexInlineTotaBot/
+echo "[+] Downloading updates from git"
 git pull
+echo "[+] Starting service"
 sudo service bot_telegram_pokedex start
